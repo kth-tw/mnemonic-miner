93cd64f124f5093f976950523e8b6c5ce292d0ee
kth-tw <me@kth.tw>
2023-12-15 21:30:37 +0800

    mine the wallet prefix

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello, world')
+import crypto from 'node:crypto'
+import * as ethers from 'ethers'
+
+const prefix = ['0000', '1234', 'abcd', '0123']
+const length = 24
+while (true) {
+  const mneonic = ethers.Mnemonic.fromEntropy(crypto.randomBytes(length / 3 * 4))
+  const { address } = ethers.HDNodeWallet.fromMnemonic(mneonic)
+  console.log(address)
+  if (prefix.some(s => RegExp(`^0x${s}`, 'i').test(address))) {
+    console.log(mneonic.phrase)
+    console.log(address)
+    break
+  }
+}
