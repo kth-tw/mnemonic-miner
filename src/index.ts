4816f90b70208358da4227e3ba0731cdc9e63156
kth-tw <me@kth.tw>
2023-12-16 01:45:37 +0800

    feat: do not break

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,48 @@ import crypto from 'node:crypto'
 import * as ethers from 'ethers'
 import { CoolWalletWordList } from './coolwallet'
 
-const prefix = ['0000', '1234', 'abcd', '0123']
+const prefix = [
+  '000000',
+  '111111',
+  '222222',
+  '333333',
+  '444444',
+  '555555',
+  '666666',
+  '777777',
+  '888888',
+  '999999',
+  'aaaaaa',
+  'bbbbbb',
+  'cccccc',
+  'dddddd',
+  'eeeeee',
+  'ffffff',
+  '012345',
+  '123456',
+  'abcdef',
+]
 const length = 24
-const batchSize = 1000
+// const batchSize = 1000
 while (true) {
-  let finish = false
-  const start = (new Date()).getTime()
-  for (let i = 0; i < batchSize; i += 1) {
-    const mneonic = ethers.Mnemonic.fromEntropy(crypto.randomBytes(length / 3 * 4))
-    const { address } = ethers.HDNodeWallet.fromMnemonic(mneonic)
-    // console.log(address)
-    if (prefix.some(s => RegExp(`^0x${s}`, 'i').test(address))) {
-      console.log(`address: ${address}`)
-      console.log(`en: ${mneonic.phrase}`)
-      console.log(`CoolWallet: ${ethers.Mnemonic.fromEntropy(mneonic.entropy, undefined, new CoolWalletWordList()).phrase}`)
-      finish = true
-      break
-    }
+  // const finish = false
+  // const start = (new Date()).getTime()
+  // for (let i = 0; i < batchSize; i += 1) {
+  const mneonic = ethers.Mnemonic.fromEntropy(crypto.randomBytes(length / 3 * 4))
+  const { address } = ethers.HDNodeWallet.fromMnemonic(mneonic)
+  if (prefix.some(s => RegExp(`^0x${s}`,
+    'i').test(address))) {
+    console.log(`address: ${address}`)
+    console.log(`en: ${mneonic.phrase}`)
+    console.log(`CoolWallet: ${ethers.Mnemonic.fromEntropy(mneonic.entropy, undefined, new CoolWalletWordList()).phrase}`)
+    console.log('-----------------------------------------------------------------------------------------------------------------------------------------------------------')
+    // finish = true
+    // break
   }
-  const end = (new Date()).getTime()
-  console.log(`mine ${batchSize} address, rate = ${1000 / (end - start) * 1000} / s`)
-  if (finish) {
-    break
-  }
+  // }
+  // const end = (new Date()).getTime()
+  // console.log(`mine ${batchSize} address, rate = ${1000 / (end - start) * 1000} / s`)
+  // if (finish) {
+  //   break
+  // }
 }
