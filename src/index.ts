15476bad86d5afa9f8a738887ddb230b8682997d
kth-tw <me@kth.tw>
2023-12-15 22:18:19 +0800

    feat: rate

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,25 @@ import { CoolWalletWordList } from './coolwallet'
 
 const prefix = ['0000', '1234', 'abcd', '0123']
 const length = 24
+const batchSize = 1000
 while (true) {
-  const mneonic = ethers.Mnemonic.fromEntropy(crypto.randomBytes(length / 3 * 4))
-  const { address } = ethers.HDNodeWallet.fromMnemonic(mneonic)
-  console.log(address)
-  if (prefix.some(s => RegExp(`^0x${s}`, 'i').test(address))) {
-    console.log(`address: ${address}`)
-    console.log(`en: ${mneonic.phrase}`)
-    console.log(`CoolWallet: ${ethers.Mnemonic.fromEntropy(mneonic.entropy, undefined, new CoolWalletWordList()).phrase}`)
+  let finish = false
+  const start = (new Date()).getTime()
+  for (let i = 0; i < batchSize; i += 1) {
+    const mneonic = ethers.Mnemonic.fromEntropy(crypto.randomBytes(length / 3 * 4))
+    const { address } = ethers.HDNodeWallet.fromMnemonic(mneonic)
+    // console.log(address)
+    if (prefix.some(s => RegExp(`^0x${s}`, 'i').test(address))) {
+      console.log(`address: ${address}`)
+      console.log(`en: ${mneonic.phrase}`)
+      console.log(`CoolWallet: ${ethers.Mnemonic.fromEntropy(mneonic.entropy, undefined, new CoolWalletWordList()).phrase}`)
+      finish = true
+      break
+    }
+  }
+  const end = (new Date()).getTime()
+  console.log(`mine ${batchSize} address, rate = ${1000 / (end - start) * 1000} / s`)
+  if (finish) {
     break
   }
 }
